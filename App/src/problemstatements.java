80d96284459509786d019144d2da7e083b8728db
yaswanthrajurallabandi-cell <yaswanthrajurallabandi@gmail.com>
2026-04-17 21:40:51 +0530

    yaswanth added ps-6

--- App/src/problemstatements.java
+++ App/src/problemstatements.java
@@ -1,30 +1,23 @@
 public class problemstatements {
 
-    static int linear(String[] arr, String key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(key)) return i;
-        }
-        return -1;
+    static int floor(int[] arr, int x) {
+        int res = -1;
+        for (int v : arr)
+            if (v <= x) res = v;
+        return res;
     }
 
-    static int binary(String[] arr, String key) {
-        int l = 0, h = arr.length - 1;
-
-        while (l <= h) {
-            int m = (l + h) / 2;
-
-            if (arr[m].equals(key)) return m;
-            else if (arr[m].compareTo(key) < 0) l = m + 1;
-            else h = m - 1;
-        }
+    static int ceiling(int[] arr, int x) {
+        for (int v : arr)
+            if (v >= x) return v;
         return -1;
     }
 
     public static void main(String[] args) {
 
-        String[] arr = {"accA", "accB", "accB", "accC"};
+        int[] arr = {10, 25, 50, 100};
 
-        System.out.println("Linear: " + linear(arr, "accB"));
-        System.out.println("Binary: " + binary(arr, "accB"));
+        System.out.println("Floor(30): " + floor(arr, 30));
+        System.out.println("Ceiling(30): " + ceiling(arr, 30));
     }
 }
