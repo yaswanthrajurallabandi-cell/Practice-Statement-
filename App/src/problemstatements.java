3c259359239c036dde037475e5cc8b6095b1ad36
yaswanthrajurallabandi-cell <yaswanthrajurallabandi@gmail.com>
2026-04-17 21:39:15 +0530

    yaswanth added ps-5

--- App/src/problemstatements.java
+++ App/src/problemstatements.java
@@ -1,41 +1,30 @@
-import java.util.*;
-
-class Asset {
-    String name;
-    double returnRate;
-
-    Asset(String name, double returnRate) {
-        this.name = name;
-        this.returnRate = returnRate;
-    }
-
-    public String toString() {
-        return name + ":" + returnRate;
-    }
-}
-
 public class problemstatements {
 
-    // Merge Sort (ASC)
-    static void mergeSort(List<Asset> list) {
-        list.sort(Comparator.comparingDouble(a -> a.returnRate));
-        System.out.println("Merge Sort ASC: " + list);
+    static int linear(String[] arr, String key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(key)) return i;
+        }
+        return -1;
     }
 
-    // Quick Sort (DESC)
-    static void quickSort(List<Asset> list) {
-        list.sort((a, b) -> Double.compare(b.returnRate, a.returnRate));
-        System.out.println("Quick Sort DESC: " + list);
+    static int binary(String[] arr, String key) {
+        int l = 0, h = arr.length - 1;
+
+        while (l <= h) {
+            int m = (l + h) / 2;
+
+            if (arr[m].equals(key)) return m;
+            else if (arr[m].compareTo(key) < 0) l = m + 1;
+            else h = m - 1;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
 
-        List<Asset> list = new ArrayList<>();
-        list.add(new Asset("AAPL", 12));
-        list.add(new Asset("TSLA", 8));
-        list.add(new Asset("GOOG", 15));
+        String[] arr = {"accA", "accB", "accB", "accC"};
 
-        mergeSort(new ArrayList<>(list));
-        quickSort(new ArrayList<>(list));
+        System.out.println("Linear: " + linear(arr, "accB"));
+        System.out.println("Binary: " + binary(arr, "accB"));
     }
 }
